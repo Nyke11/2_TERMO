c1af39f45889c60b1af2d7a457aa7fa7cb6f331c
Nyke11 <monykearaujo11@gmail.com>
2026-08-19 13:51:01 -0300

    19.08.2026

---
+++ BACKEND/AULA10/ex3.js
@@ -0,0 +1,12 @@
+const alunos = ["Ana", "Bruno", "Carlos", "Enzo"];
+
+console.log("Lista de Alunos: ");
+console.log(alunos);
+
+console.log(`Primeiro aluno: ${alunos[0]}`);
+console.log(`Primeiro aluno: ${alunos[1]}`);
+console.log(`Quantidades de alunos: ${alunos.length}`);
+
+// Acrescentar mais 2 nomes ao array
+// Mostrar o terceiro aluno
+// Mostrar o último aluno
